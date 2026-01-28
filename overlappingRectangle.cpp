1e5143b738eae79bd4d2d786ef93ed39bd8b0385
Sunny Dutta <sunnyrizzler123@gmail.com>
2026-01-28 20:11:53 +0530

    Create overlappingRectangle.cpp

---
+++ overlappingRectangle.cpp
@@ -0,0 +1,19 @@
+//Given two rectangles,find if the given rectangles overlap or not.
+//There is a Simple approach.Two rectangles do not overlap if one of the following Condition is True.
+//1.One rectangle is above top edge of other rectangle.
+//2.One rectangle is on the left side of the left edge of other rectangle.
+#include <iostream>
+using namespace std;
+struct Point{
+    int x,y;
+};
+bool doOverlap(Point l1,Point r1,Point l2,Point r2){
+  if(l1.x > r2.x || l2.x > r1.x) return false;
+  if(r1.y > l2.y || r2.y > l1.y) return false;
+return true;
+}
+int main(){
+cin >> &Point->l1 >> &Point->r1 >> &Point->l2 >> &Point->r2;
+cout << doOverlap(l1,r1,l2,r2) << endl;
+return 0;
+}
